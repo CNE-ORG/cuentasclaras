0a91f1a694a60d8d288a0d13b7535339a50dc3ca
CONSEJO NACIONAL ELECTORAL CNE <153869590+analistafinanciero@users.noreply.github.com>
2024-09-04 15:34:24 -0500

    Add files via upload

--- patrimoniofa.py
+++ patrimoniofa.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import matplotlib.patches as mpatches
 import streamlit as st
-from src.reportes import reporte1, reporte2, reporte3, reporte4, reporte1c, reporte2c, todos, varios, todosc, open_pdf
+from src.reportes import reporte1, reporte2, reporte3, reporte4, reporte5, reporte6, reporte_mejorado, reporte1c, reporte2c, todos, varios, varios1, todosc, todosc1, open_pdf
 from src.cargar import load_data1, load_data2, load_data3 
 from streamlit_option_menu import option_menu
 from typing import List, Tuple
@@ -35,6 +35,7 @@ def write():
     url = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/organizaciones_patrimonio.xlsx'
     url2 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/candidatos_consolidado.xlsx'
     url3 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/descripciones.xlsx'
+    logo = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/cne.jpg'
 
     df, df2, df3 = load_and_cache_data(url, url2, url3)
     
@@ -82,6 +83,123 @@ def write():
             st.write(df_filtrado.head(10))
 
     elif selected == "Organizacion":
+        
+            # Filtros interactivos
+            filtro_grupo = st.sidebar.selectbox('Filtrar por Agrupacion Politica', df['nombre_agrupacion_politica'].unique())
+            df_filtrado = df[df['nombre_agrupacion_politica'] == filtro_grupo] if filtro_grupo != 'Todos' else df
+            
+            # Unir con el archivo de descripciones
+            df_filtrado = df_filtrado.merge(df3, on='codigo', how='left')
+            dataset = df_filtrado
+
+            # Agrupar los datos por tipo
+            data_agrupada = df_filtrado.groupby(['tipo', 'codigo', 'nombre', 'descripcion']).agg({'valor': 'sum'}).reset_index()
+
+            # Filtrar ingresos y egresos
+            ingresos_df = data_agrupada[data_agrupada['tipo'] == 1]
+            egresos_df = data_agrupada[data_agrupada['tipo'] == 2]
+
+            # Calcular totales
+            total_ingresos = ingresos_df['valor'].sum()
+            total_egresos = egresos_df['valor'].sum()
+
+            st.write("## Otros informes")
+            inputss = st.multiselect("Cuales Informes desea descargar?", ["Todos", "DECLARACION DE PATRIMONIO, INGRESOS Y GASTOS ANUALES", "INFORME DE INGRESOS Y GASTOS ESTATUTO DE LA OPOSICIÓN", "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011"])
+            st.write(inputss) 
+            
+            
+            # Si se selecciona "Todos"
+            if "Todos" in inputss:
+                pdfs = []
+                todos(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, logo)
+            else:
+                # Lista para almacenar las rutas de los PDFs generados
+                pdfs = []
+
+                # Generar los informes seleccionados
+                if "DECLARACION DE PATRIMONIO, INGRESOS Y GASTOS ANUALES" in inputss:
+                    informe = inputss
+                    pdf_path5 = "reporte5.pdf"
+                    reporte5(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdf_path5, logo, informe)
+                    pdfs.append(pdf_path5)
+                    
+                if "INFORME DE INGRESOS Y GASTOS ESTATUTO DE LA OPOSICIÓN" in inputss:
+                    informe = inputss
+                    pdf_path2 = "reporte2.pdf"
+                    reporte6(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdf_path2, logo, informe)
+                    pdfs.append(pdf_path2)
+                    
+                if "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011" in inputss:
+                    informe = inputss
+                    pdf_path3 = "reporte3.pdf"
+                    reporte_mejorado(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdf_path3, logo, informe)
+                    pdfs.append(pdf_path3)                    
+
+        # Si se seleccionó más de un informe, combinarlos
+                if len(pdfs) > 1:
+                    varios1(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdfs, logo)
+                elif len(pdfs) == 1:
+                    open_pdf(pdfs[0])  
+
+    
+
+    elif selected == "Candidatos":
+            
+            
+            # Filtros interactivos
+            filtro_grupo = st.sidebar.selectbox('Filtrar por Agrupacion Politica', df['nombre_agrupacion_politica'].unique())
+            
+            # filtrar el candidato
+            df_filtradoc = df2[df2['nombre'] == filtro_grupo]
+            
+            # Unir con el archivo de descripciones
+            df_filtradoc = df_filtradoc.merge(df3, on='codigo', how='left')
+            
+            filtro_corporacion = st.sidebar.selectbox('Filtrar por Corporación', df_filtradoc['nombre_corporacion'].unique())
+            df_filtradocor = df_filtradoc[df_filtradoc['nombre_corporacion'] == filtro_corporacion]
+            
+            filtro_candidato = st.sidebar.selectbox('Filtrar por Candidato', df_filtradocor['nombre_completo'].unique())
+            df_filtradocan = df_filtradocor[df_filtradocor['nombre_completo'] == filtro_candidato]
+            datasetc = df_filtradocan
+
+            data_agrupadac = df_filtradocan.groupby(['tipo', 'codigo', 'nombre', 'descripcion']).agg({'valor': 'sum'}).reset_index()
+
+            # Filtrar ingresos y egresos
+            ingresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 1]
+            egresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 2]
+
+            # Calcular totales
+            total_ingresosc = ingresos_dfc['valor'].sum()
+            total_egresosc = egresos_dfc['valor'].sum()
+        
+            st.write("## Informes Candidatos")
+            inputsc = st.multiselect("Cuales Informes desea descargar?", ["Todos", "INFORME INDIVIDUAL DE INGRESOS Y GASTOS DE LA CAMPAÑA", "Otros"])
+            st.write(inputsc) 
+            
+            # Si se selecciona "Todos"
+            if "Todos" in inputsc:
+                pdfs = []
+                todosc(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc)
+            else:
+                # Lista para almacenar las rutas de los PDFs generados
+                pdfs = []
+
+                # Generar los informes seleccionados
+                if "INFORME INDIVIDUAL DE INGRESOS Y GASTOS DE LA CAMPAÑA" in inputsc:
+                    pdf_path1 = "reporte1c.pdf"
+                    reporte1c(datasetc,ingresos_dfc, egresos_dfc, total_ingresosc, total_egresosc, pdf_path1, logo)
+                    pdfs.append(pdf_path1)
+                   
+                if "Otros" in inputsc:
+                    pdf_path3 = "reporte2c.pdf"
+                    reporte2c(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc, pdf_path3)
+                    pdfs.append(pdf_path3)                    
+
+        # Si se seleccionó más de un informe, combinarlos
+            if len(pdfs) >= 1:
+                todosc1(datasetc, ingresos_dfc, egresos_dfc, total_ingresosc, total_egresosc, pdfs, logo )
+            
+    elif selected == "Otros Informes":
 
             # Filtros interactivos
             filtro_grupo = st.sidebar.selectbox('Filtrar por Agrupacion Politica', df['nombre_agrupacion_politica'].unique())
@@ -135,89 +253,8 @@ def write():
                 if len(pdfs) > 1:
                     varios(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdfs)
                 elif len(pdfs) == 1:
-                    open_pdf(pdfs[0])
-
-    elif selected == "Candidatos":
-            
-            
-            # Filtros interactivos
-            filtro_grupo = st.sidebar.selectbox('Filtrar por Agrupacion Politica', df['nombre_agrupacion_politica'].unique())
-            
-            # filtrar el candidato
-            df_filtradoc = df2[df2['nombre'] == filtro_grupo]
-            
-            # Unir con el archivo de descripciones
-            df_filtradoc = df_filtradoc.merge(df3, on='codigo', how='left')
-            
-            filtro_corporacion = st.sidebar.selectbox('Filtrar por Corporación', df_filtradoc['nombre_corporacion'].unique())
-            df_filtradocor = df_filtradoc[df_filtradoc['nombre_corporacion'] == filtro_corporacion]
-            
-            filtro_candidato = st.sidebar.selectbox('Filtrar por Candidato', df_filtradocor['nombre_completo'].unique())
-            df_filtradocan = df_filtradocor[df_filtradocor['nombre_completo'] == filtro_candidato]
-            datasetc = df_filtradocan
-
-            data_agrupadac = df_filtradocan.groupby(['tipo', 'codigo', 'nombre', 'descripcion']).agg({'valor': 'sum'}).reset_index()
-
-            # Filtrar ingresos y egresos
-            ingresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 1]
-            egresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 2]
-
-            # Calcular totales
-            total_ingresosc = ingresos_dfc['valor'].sum()
-            total_egresosc = egresos_dfc['valor'].sum()
-        
-            st.write("## Informes Candidatos")
-            inputsc = st.multiselect("Cuales Informes desea descargar?", ["Todos", "INFORME INDIVIDUAL DE INGRESOS Y GASTOS DE LA CAMPAÑA", "Otros"])
-            st.write(inputsc) 
-            
-            # Si se selecciona "Todos"
-            if "Todos" in inputsc:
-                pdfs = []
-                todosc(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc)
-            else:
-                # Lista para almacenar las rutas de los PDFs generados
-                pdfs = []
-
-                # Generar los informes seleccionados
-                if "INFORME INDIVIDUAL DE INGRESOS Y GASTOS DE LA CAMPAÑA" in inputsc:
-                    pdf_path1 = "reporte1c.pdf"
-                    reporte1c(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc, pdf_path1)
-                    pdfs.append(pdf_path1)
-                   
-                if "Otros" in inputsc:
-                    pdf_path3 = "reporte2c.pdf"
-                    reporte2c(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc, pdf_path3)
-                    pdfs.append(pdf_path3)                    
-
-        # Si se seleccionó más de un informe, combinarlos
-            if len(pdfs) >= 1:
-                varios(datasetc, ingresos_dfc, egresos_dfc, total_ingresosc, total_egresosc, pdfs)
-            
-            
-    elif selected == "Otros Informes":
-        
-            filtro_grupo = st.sidebar.selectbox('Filtrar por Agrupacion Politica', df['nombre_agrupacion_politica'].unique())       
-            df_filtradoc = df2[df2['nombre'] == filtro_grupo]
-            
-            # Unir con el archivo de descripciones
-            df_filtradoc = df_filtradoc.merge(df3, on='codigo', how='left')
-            
-            datasetc = df_filtradoc
-            filtro_candidato = st.sidebar.selectbox('Filtrar por Candidato', df_filtradoc['nombre_completo'].unique())
-            df_filtradoc = df_filtradoc[df_filtradoc['nombre_completo'] == filtro_candidato]
-
-            data_agrupadac = df_filtradoc.groupby(['tipo', 'codigo', 'nombre', 'descripcion']).agg({'valor': 'sum'}).reset_index()
-
-            # Filtrar ingresos y egresos
-            ingresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 1]
-            egresos_dfc = data_agrupadac[data_agrupadac['tipo'] == 2]
-
-            # Calcular totales
-            total_ingresosc = ingresos_dfc['valor'].sum()
-            total_egresosc = egresos_dfc['valor'].sum()
-        
-            pdf_path1 = "reporte4.pdf"
-            reporte4(datasetc,ingresos_dfc,egresos_dfc,total_ingresosc,total_egresosc, pdf_path1)            
+                    open_pdf(pdfs[0])        
+       
 
 if __name__ == "__main__":
     write()

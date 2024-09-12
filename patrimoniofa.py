d734b3094265ca04e94b511b9dd7b7c931bc21e4
CONSEJO NACIONAL ELECTORAL CNE <153869590+analistafinanciero@users.noreply.github.com>
2024-09-12 14:39:59 -0500

    Add files via upload

--- patrimoniofa.py
+++ patrimoniofa.py
@@ -5,20 +5,22 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import matplotlib.patches as mpatches
 import streamlit as st
-from src.reportes import reporte1, reporte2, reporte3, reporte1c, reporte4c, todos, varios1, todosc, todosc1, open_pdf
-from src.cargar import load_data1, load_data2, load_data3, load_data4, load_data5 
+from src.reportes import reporte1, reporte2, reporte3, reporte4, reporte1c, reporte4c, todos, varios1, todosc, todosc1, open_pdf
+from src.cargar import load_data1, load_data2, load_data3, load_data4, load_data5, load_data6, load_data7 
 from streamlit_option_menu import option_menu
 from typing import List, Tuple
 
 
 @st.cache_data
-def load_and_cache_data(url1: str, url2: str, url3: str, url4: str, url5: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def load_and_cache_data(url1: str, url2: str, url3: str, url4: str, url5: str, url6: str, url7: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     df = load_data1(url1)
     df2 = load_data2(url2)
     df3 = load_data3(url3)
     df4 = load_data4(url4)
     df5 = load_data5(url5)
-    return df, df2, df3, df4, df5
+    df6 = load_data6(url6)
+    df7 = load_data7(url7)
+    return df, df2, df3, df4, df5, df6, df7
 
 def calculate_kpis(df: pd.DataFrame) -> List[float]:
     valor_total = df['valor'].sum()
@@ -39,9 +41,11 @@ def write():
     url3 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/descripcionesFun.xlsx'
     url4 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/gastos_destinados.xlsx'
     url5 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/descripcionesDes.xlsx'
+    url6 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/ingresos_destinados.xlsx'
+    url7 = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/descripcionesDes.xlsx'
     logo = 'https://raw.githubusercontent.com/CNE-ORG/cuentasclaras/main/data/cne.jpg'
 
-    df, df2, df3, df4, df5 = load_and_cache_data(url, url2, url3, url4, url5)
+    df, df2, df3, df4, df5, df6, df7 = load_and_cache_data(url, url2, url3, url4, url5, url6, url7)
     
     # menu lateral
     with st.sidebar:
@@ -108,7 +112,6 @@ def write():
             total_egresos = egresos_df['valor'].sum()
             
             # otra BD gastos destinados
-            # Filtros interactivos
             df_filtradog = df4[df4['nombre_agrupacion_politica'] == filtro_grupo] if filtro_grupo != 'Todos' else df
             
             # Unir con el archivo de descripciones
@@ -126,16 +129,30 @@ def write():
             total_ingresosg = ingresos_dfg['valor'].sum()
             total_egresosg = egresos_dfg['valor'].sum()
             
-
-            st.write("## Otros informes")
-            inputss = st.multiselect("Cuales Informes desea descargar?", ["Todos", "DECLARACION DE PATRIMONIO, INGRESOS Y GASTOS ANUALES", "INFORME DE INGRESOS Y GASTOS ESTATUTO DE LA OPOSICIÓN", "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011"])
-            st.write(inputss) 
+            # otra BD ingresos destinados
+            df_filtradoi = df6[df6['nombre_agrupacion_politica'] == filtro_grupo] if filtro_grupo != 'Todos' else df
+            dataseti = df_filtradoi
             
+            # Agrupar los datos por tipo
+            data_agrupadai = dataseti.groupby(['tipo', 'codigo', 'concepto', 'persona', 'cedula', 'telefono', 'direccion', 'acta']).agg({'valor': 'sum'}).reset_index()
+            
+            # Filtrar ingresos y egresos
+            ingresos_dfi = dataseti[dataseti['tipo'] == 1]
+            egresos_dfi = dataseti[dataseti['tipo'] == 2]
+
+            # Calcular totales
+            total_ingresosi = ingresos_dfi['valor'].sum()
+            total_egresosi = egresos_dfi['valor'].sum()
+            
+
+            st.write("## Informes de Organización")
+            inputss = st.multiselect("Cuales Informes desea descargar?", ["Todos", "DECLARACION DE PATRIMONIO, INGRESOS Y GASTOS ANUALES", "INFORME DE INGRESOS Y GASTOS ESTATUTO DE LA OPOSICIÓN", "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011", "CONTRIBUCIONES, DONACIONES Y CREDITOS, EN DINERO O EN ESPECIE, DE SUS AFILIADOS Y/O DE PARTICULARES"])
+            st.write(inputss) 
             
             # Si se selecciona "Todos"
             if "Todos" in inputss:
                 pdfs = []
-                todos(dataset, datasetg, ingresos_df, ingresos_dfg, egresos_df, egresos_dfg, total_ingresos, total_ingresosg, total_egresos, total_egresosg, logo)
+                todos(dataset, datasetg, dataseti, ingresos_df, ingresos_dfg, ingresos_dfi, egresos_df, egresos_dfg, egresos_dfi, total_ingresos, total_ingresosg, total_ingresosi, total_egresos, total_egresosg, total_egresosi, logo)
             else:
                 # Lista para almacenar las rutas de los PDFs generados
                 pdfs = []
@@ -152,14 +169,22 @@ def write():
                     informe = inputss
                     pdf_path2 = "reporte2.pdf"
                     reporte2(dataset, ingresos_df, egresos_df, total_ingresos, total_egresos, pdf_path2, logo, informe)
+                    st.write(data_agrupada)
                     pdfs.append(pdf_path2)
                 
                 if "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011" in inputss:
                     informe = inputss
                     pdf_path3 = "reporte3.pdf"
                     reporte3(datasetg, ingresos_dfg, egresos_dfg, total_ingresosg, total_egresosg, pdf_path3, logo, informe)
-                    st.write(data_agrupadag)
-                    pdfs.append(pdf_path3)                    
+                    st.write(df_filtradog)
+                    pdfs.append(pdf_path3)
+
+                if "CONTRIBUCIONES, DONACIONES Y CREDITOS, EN DINERO O EN ESPECIE, DE SUS AFILIADOS Y/O DE PARTICULARES" in inputss:
+                    informe = inputss
+                    pdf_path4 = "reporte4.pdf"
+                    reporte4(dataseti, ingresos_dfi, egresos_dfi, total_ingresosi, total_egresosi, pdf_path4, logo, informe)
+                    st.write(ingresos_dfi)
+                    pdfs.append(pdf_path4)                    
 
         # Si se seleccionó más de un informe, combinarlos
                 if len(pdfs) > 1:
@@ -242,7 +267,7 @@ def write():
             total_ingresos = ingresos_df['valor'].sum()
             total_egresos = egresos_df['valor'].sum()
 
-            st.write("## Informes de Organización")
+            st.write("## Otros Informes")
             inputss = st.multiselect("Cuales Informes desea descargar?", ["Todos", "DECLARACION DE PATRIMONIO, INGRESOS Y GASTOS ANUALES", "INFORME DE INGRESOS Y GASTOS ESTATUTO DE LA OPOSICIÓN", "GASTOS DESTINADOS PARA ACTIVIDADES CONTEMPLADAS EN EL ARTICULO 18 DE LA LEY 1475 DE 2011"])
             st.write(inputss) 
             
